5c3220613cf134234d98150fb0f847c30b16adb7
jamshid6573 <jamshid125200@gmail.com>
2023-05-23 17:42:01 +0500

    2.0

--- users/models.py
+++ users/models.py
@@ -5,9 +5,23 @@ from django.contrib.auth.models import AbstractUser
 
 
 class UsersAb(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+    YT = 'youtuber'
+    ROLES = [
+        (USER, 'user'),
+        (ADMIN, 'admin'),
+        (YT, 'youtuber'),
+    ]
+    role = models.CharField(max_length=255, choices=ROLES, default=USER, blank=True)
     gold = models.FloatField(default=0, null=True, blank=True)
+    sum = models.FloatField(default=0, null=True, blank=True)
     image = models.ImageField(default=None, null=True, blank=True)
     total_gold = models.FloatField(default=0, null=True, blank=True)
+    open_box = models.IntegerField(default=0, null=True, blank=True)
+    tg = models.CharField(max_length=255, null=True, blank=True)
+    number = models.CharField(max_length=255, null=True, blank=True)
+    is_ban = models.BooleanField(default=False)
 
     @property
     def imageURL(self):
